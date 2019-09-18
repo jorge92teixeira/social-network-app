f26258f2c75d3ad016da53c927904d959c3a1518
Jorge Teixeira <jorge92@mailbox.org>
2019-09-18 16:24:41 +0100

    delete post by Id api route (closes #24)

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -64,4 +64,31 @@ router.get('/:id', auth, async (req, res) => {
     return res.status(500).send('Server Error');
   }
 });
+
+// @route   Delete /api/posts/:id
+// @desc    Delete post by Id
+// @access  Private
+router.delete('/:id', auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (!post) {
+      return res.status(404).json({ msg: 'Post not found' });
+    }
+    // Check user
+    if (post.user.toString() !== req.user.id) {
+      return res.status(401).json({ msg: 'User not authorized' });
+    }
+
+    await post.remove();
+
+    return res.json({ msg: 'Post removed' });
+  } catch (error) {
+    console.error(error.message);
+    if (error.kind === 'ObjectId') {
+      return res.status(404).json({ msg: 'Post not found' });
+    }
+    return res.status(500).send('Server Error');
+  }
+});
+
 module.exports = router;
