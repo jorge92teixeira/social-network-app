7eb1ec6c17a98e1fcb5a313f3c3725674fc818ac
Jorge Teixeira <jorge92@mailbox.org>
2019-09-18 16:13:20 +0100

    get post by Id api route (closes #23)

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -46,5 +46,22 @@ router.get('/', auth, async (req, res) => {
   }
 });
 
-
+// @route   GET /api/posts/:id
+// @desc    Get post by Id
+// @access  Private
+router.get('/:id', auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (!post) {
+      return res.status(404).json({ msg: 'Post not found' });
+    }
+    return res.json(post);
+  } catch (error) {
+    console.error(error.message);
+    if (error.kind === 'ObjectId') {
+      return res.status(404).json({ msg: 'Post not found' });
+    }
+    return res.status(500).send('Server Error');
+  }
+});
 module.exports = router;
