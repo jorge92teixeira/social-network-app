0fbc75bc0b5c18e421c4f024eb78f26f7767b2db
Jorge Teixeira <jorge92@mailbox.org>
2019-09-20 22:11:03 +0100

    Alert component (closes #37)

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import NavBar from './components/layout/NavBar';
 import Landing from './components/layout/Landing';
 import Login from './components/auth/Login';
 import Register from './components/auth/Register';
+import Alert from './components/layout/Alert';
 // Redux
 import { Provider } from 'react-redux';
 import store from './store';
@@ -17,6 +18,7 @@ const App = () => (
         <NavBar />
         <Route exact path='/' component={Landing} />
         <section className="container">
+          <Alert />
           <Switch>
             <Route exact path='/register' component={Register} />
             <Route exact path='/login' component={Login} />
