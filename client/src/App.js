76001f11044cbc3cb0fda46a1c75dfbc12153380
Jorge Teixeira <jorge92@mailbox.org>
2019-09-20 12:39:49 +0100

    Navbar and Landing page navigation, Register component (closes #30, closes #31, #closes #32, closes #34)

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,24 @@
 import React, { Fragment } from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './App.css';
 import NavBar from './components/layout/NavBar';
 import Landing from './components/layout/Landing';
+import Login from './components/auth/Login';
+import Register from './components/auth/Register';
 
 const App = () => (
+  <BrowserRouter>
     <Fragment>
       <NavBar />
-      <Landing />
+      <Route exact path='/' component={Landing} />
+      <section className="container">
+        <Switch>
+          <Route exact path='/register' component={Register} />
+          <Route exact path='/login' component={Login} />
+        </Switch>
+      </section>
     </Fragment>
+  </BrowserRouter>
 );
 
 export default App;
