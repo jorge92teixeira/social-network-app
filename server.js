2b84a3404d5517fd1e51bbdb92a9736f0b7a05b2
Jorge Teixeira <jorge92@mailbox.org>
2019-10-07 18:20:21 +0100

    ready for deployment

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 const connectDB = require('./config/db');
 const env = require('./config/env');
 
@@ -15,6 +16,16 @@ app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/posts', require('./routes/api/posts'));
 app.use('/api/profile', require('./routes/api/profile'));
+
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 app.listen(env.PORT, () => {
   console.log(`Server running on port ${env.PORT}`);
